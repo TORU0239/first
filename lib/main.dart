5651efb99a56fced1b4fcb329f470fda9aa41a41
Toru Choi <blueocean239@gmail.com>
2022-02-06 18:42:59 +0800

    feat: infinite list app baed on the official docs of Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MainApplication());
@@ -9,20 +10,58 @@ class MainApplication extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     // Adding main page design
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          actions: [Icon(Icons.star), Icon(Icons.star),],
-          title: const Text("Flutter")),
-        body: SizedBox(
-          child: ElevatedButton(
-            child: Text('Button!'),
-            onPressed: (){}
-            )
-          )
-        )
-      );
+    return const MaterialApp(
+        title: 'Startup Name Generator',
+        home: RandomWords(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator')
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
